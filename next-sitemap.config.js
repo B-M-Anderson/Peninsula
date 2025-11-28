ce411447f8dd1dc3a57af6258def5713be04b6f5
B-M-Anderson <bennetta32.30@gmail.com>
2025-11-27 18:46:37 -0600

    sitemap etc. trying to fix 'suspicious site stuff'

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.bennettanderson.com',
+  generateRobotsTxt: true, // optional
+  sitemapSize: 5000, // max URLs per sitemap
+}
